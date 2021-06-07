b9d225bcbefff370a4d26e6384c4103636738eaa
ricot <ricot@DESKTOP-FTHKA14>
2021-06-07 19:19:20 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TP2_EJ15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Buenass");
+            Console.Title = "Número negativo o positivo";
+
+            float Número;
+
+            Console.Write("Ingrese el Número a verificar: ");
+            Número = Convert.ToSingle(Console.ReadLine());
+
+            if (Número >= 0)
+
+                Console.Write($"El Número es positivo");
+
+            else
+                Console.Write("El Número es negativo");
+        }
+    }
+}
